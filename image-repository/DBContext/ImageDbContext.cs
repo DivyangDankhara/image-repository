ddbd8e0300f0373330474c9b5bb6dc53b56d5869
Divyang <divyangdankhara007@outlook.com>
2021-01-17 00:54:33 -0500

    Repo init, add image upload, edit, delete, and details.

---
+++ image-repository/DBContext/ImageDbContext.cs
@@ -0,0 +1,14 @@
+﻿using image_repository.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace image_repository.DBContext {
+  public class ImageDbContext:DbContext {
+    public ImageDbContext(DbContextOptions<ImageDbContext> options) : base(options) { }
+
+    public DbSet<ImageModel> Images { get; set; }
+  }
+}
---
+++ image-repository/Models/ImageModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace image_repository.Models {
+  public class ImageModel {
+    [Key]
+    public int ImageId { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string Title { get; set; }
+
+    [Column(TypeName = "nvarchar(100)")]
+    [DisplayName("Image Name")]
+    public string ImageName { get; set; }
+
+    [Required]
+    [NotMapped]
+    [DisplayName("Upload File")]
+    public IFormFile ImageFile { get; set; }
+  }
+}
