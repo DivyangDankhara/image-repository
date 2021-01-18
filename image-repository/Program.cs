3da0c73022af2663842e226cfc1ae98c2b62264e
Divyang <divyangdankhara007@outlook.com>
2021-01-17 21:10:08 -0500

    fix style

--- image-repository/Program.cs
+++ image-repository/Program.cs
@@ -7,20 +7,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace image_repository
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+namespace image_repository {
+  public class Program {
+    public static void Main(string[] args) {
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
