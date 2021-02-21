7d9945f5f2216758d6c9dc4f1afc17ae335f4063
Divyang <divyangdankhara007@outlook.com>
2021-02-20 21:32:58 -0500

    added Json properties to model to used it in API.

--- image-repository/Models/ImageModel.cs
+++ image-repository/Models/ImageModel.cs
@@ -1,26 +1,32 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace image_repository.Models {
+  [JsonObject]
   public class ImageModel {
-    
+
+    [JsonProperty]
     public int ImageId { get; set; }
 
     [Required]
+    [JsonProperty]
     [DisplayName("Image Title")]
     public string Title { get; set; }
 
-
+    [JsonProperty]
     [DisplayName("Image Name")]
     public string ImageName { get; set; }
 
     [Required]
+    [JsonIgnore]
     [DisplayName("Upload Image")]
     public IFormFile ImageFile { get; set; }
 
+    [JsonProperty]
     public string ImageDataURL { get; set; }
   }
 }
